86eef799b48d4ffcfb59e5ecbfdfe207504fa058
Bogdan <bogdanaugustynski@interia.pl>
2025-10-16 06:48:52 +0200

    chore: wyciszono 404 na /favicon.ico (pusty response)

--- korepetycje/urls.py
+++ korepetycje/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("ukryty_admin/", admin.site.urls),
