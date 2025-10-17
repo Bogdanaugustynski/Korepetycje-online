21f81840a2980cb04908ec776a485f994c34400f
Bogdan <bogdanaugustynski@interia.pl>
2025-10-17 06:21:59 +0200

    ui(księgowość): edycja danych płatności w Ali-style w edytuj_cene.html; bez pola ceny; alias widoku

--- korepetycje/urls.py
+++ korepetycje/urls.py
@@ -2,13 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
-from django.http import HttpResponse
 
 
 urlpatterns = [
     path("ukryty_admin/", admin.site.urls),
     path('', include('panel.urls')),
-    path("favicon.ico", lambda request: HttpResponse(status=204)),
 ]
 from django.conf import settings
 from django.conf.urls.static import static
