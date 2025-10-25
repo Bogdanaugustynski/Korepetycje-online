1f932afbcbdd68fc73ad6b76dd9581eede7c9dbd
Bogdan <bogdanaugustynski@interia.pl>
2025-10-25 07:33:36 +0200

    chore(deploy): wymuszenie ponownego wdrożenia tablicy

--- korepetycje/urls.py
+++ korepetycje/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 from django.http import HttpResponse  
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -10,8 +12,6 @@ urlpatterns = [
     path('', include('panel.urls')),
     path("favicon.ico", lambda request: HttpResponse(status=204)),
 ]
-from django.conf import settings
-from django.conf.urls.static import static
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
